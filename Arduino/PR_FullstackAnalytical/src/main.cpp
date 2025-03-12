6944056433ebd0b12de2c987a4cc7008d35d4430
Disguied-Coffee <danielidanque@gmail.com>
2025-03-12 10:47:36 -0500

    Init Phase 1

---
+++ Arduino/PR_FullstackAnalytical/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// char buffer to send over i2C
+char buffer[10];
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
