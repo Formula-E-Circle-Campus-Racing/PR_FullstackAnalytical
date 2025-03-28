32ea1e34ad93365d43ff1597f7d8b7694eac836f
Disguied-Coffee <danielidanque@gmail.com>
2025-03-28 14:31:06 -0500

    Removed "queryObject" idea

--- WebClient/pr-fsa-client/src/app/page.jsx
+++ WebClient/pr-fsa-client/src/app/page.jsx
@@ -1,28 +1,20 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 
-const getRecordData = async (queryObj, useStateUpdater, setError) => {
+const getRecordData = async (apiURL, useStateUpdater, setError) => {
   // validate the query object
-  if (queryObj.baseUrl === "not a url") {
-    // The base URL is null when the user first loads.
-    return;
-  }
-
-  if (!queryObj.baseUrl) {
+  if (!apiURL) {
     setError("Base URL is required!");
     return;
   }
 
-
-  // make URL to fetch data
-  const apiURL = queryObj.baseUrl; // for now.
-  console.log(apiURL);
+  // reset error
+  setError('');
 
   // make the API request
   axios.get(apiURL).then((res) => {
-    setError('');
     useStateUpdater(res.data);
   }).catch((err) => {
     useStateUpdater(null);
@@ -38,14 +30,14 @@ export default function Home() {
   // error system (verboser)
   let [error, setError] = useState(null);
 
-  // query object for making API requests
-  let [queryObj, setQueryObj] = useState({
-    baseUrl: "not a url", // random garbage to stop queries
-    limit: 5,
-    beginDate: "1-5-25",
-    endDate: "1-12-25",
-    liveUpdate: false // Feature needs to be implemented before actual usage.
-  });
+  // Old.
+  // const queryTemplate = {
+  //   baseUrl: "https://api.example.com/data",
+  //   limit: 0,
+  //   beginDate: "",
+  //   endDate: "",
+  //   liveUpdate: false
+  // }
 
   // record data (data to analyze)
   const [recordData, setRecordData] = useState(null);
@@ -53,32 +45,105 @@ export default function Home() {
   // to refresh data
   const [refresh, setRefresh] = useState(false);
 
-  // allow data to be fetched on page load
-  useEffect(() => {
-    console.info("Attempting to fetch data...");
-    console.log(queryObj)
-    getRecordData(queryObj, setRecordData, setError);
-  }, [refresh])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting form...");
+
+    let apiURL = "";
+    // assuming user selects static URL option:
+    if (!e.target[1].checked) {
+      apiURL = e.target[0].value;
+    }
+    else {
+      // build the the apiURL from form data
+
+      // for now, we'll have static keys
+      // but later we'll have dynamic ones []
+      for (let i = 0; i < e.target.length; i++) {
+        if (e.target[i].type !== "checkbox") {
+          const key = e.target[i].value;
+          console.log(key);
+        }
+      }
+      return;
+    }
+
+    getRecordData(apiURL, setRecordData, setError);
+  }
 
   return (
     <div>
       {/* https://react.dev/reference/react-dom/components/input */}
       <h1 className="text-3xl font-bold underline">Get data</h1>
-      <div>
+      <div className="flex flex-col">
         {/* Get a URL and send it */}
-        <input className="border-red-400 border-2 rounded-lg" placeholder="URL to DB API goes here" onChange={e => setQueryObj({ baseUrl: e.target.value })} />
-        <input type="checkbox" value="blah blah blah" />
-
-        <button className="ml-3 b-5" onClick={() => setRefresh(!refresh)}>Refresh</button>
         <div>
-          <h1>Data:</h1>
-          {error && <p className="text-red-500 mb-4">{error.code + ": " +error.message}</p>}
-          
-          <pre>
-            {JSON.stringify(recordData.data, null, 2)}
-          </pre>
+          <form onSubmit={handleSubmit} className="mb-4">
+            <DataQueryUI />
+          </form>
+        </div>
+        {/* present the data */}
+        <div>
+          {error && (
+            <div className="bg-red-200 text-red-800 p-2 rounded-lg">
+              <h1 className="text-lg font-bold">
+                {isAxiosError(error) ? (error.code + " (" + error.message + ")") : error}
+              </h1>
+            </div>)
+          }
+        </div>
+        <h1>Data:</h1>
+        <pre>
+          <code className="text-sm text-gray-700">
+            {recordData && JSON.stringify(recordData.data, null, 2)}
+          </code>
+        </pre>
+      </div>
+    </div >
+  );
+}
+
+const DataQueryUI = () => {
+  // determine get method
+  // (if it is checked, then just use the static URL)
+  const [dataMethod, setDataMethod] = useState(0);
+
+  return (
+    <>
+      <div className="mb-4">
+        <label className="text-lg font-bold">Static URL</label>
+        <input type="url" required="true" disabled={dataMethod} className="border-red-400 border-2 rounded-lg disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500" placeholder="URL to DB API goes here" />
+      </div>
+      <div>
+        <label className="text-lg font-bold">Use Edtior instead </label>
+        {/* Set  */}
+        <input type="checkbox" onClick={(e) => { (e.target.checked) ? setDataMethod(1) : setDataMethod(0) }} />
+        <div>
+          {/* issue with URL or 'required' checking not working. */}
+          <label type="url" required="true" className="">Base URL</label>
+          <input className="border-red-400 border-2 rounded-lg" placeholder="base URL to DB API goes here" />
+        </div>
+        <div className="mt-4">
+          {/* For now, have static keys, and later have dynamic ones */}
+          <div>
+            <label className="">Data Limit</label>
+            <input type="number" className="border-red-400 border-2 rounded-lg" placeholder="" />
+          </div>
+          <div>
+            <label className="">Begin Date</label>
+            <input type="date" className="border-red-400 border-2 rounded-lg" placeholder="" />
+          </div>
+          <div>
+            <label className="">End Date</label>
+            <input type="date" className="border-red-400 border-2 rounded-lg" placeholder="" />
+          </div>
+          <div>
+            <label className="">Live Update</label>
+            <input type="checkbox" className="border-red-400 border-2 rounded-lg" placeholder="" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+      <button className="mt-1 ml-3 b-5 bg-green-400 p-2 rounded-lg active:bg-green-600">Get Data</button>
+    </>
+  )
 }
